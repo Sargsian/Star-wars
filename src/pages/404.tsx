2bff815e7d438b15a56f7fc08ab4a67e75b1c662
unknown <mrsargsi@gmail.com>
2023-03-14 04:36:18 +0300

    fixed bugs

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,10 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import type { MouseEvent } from "react";
-import { useTranslation } from 'next-i18next'
+import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-export const getStaticProps = async ({ locale = 'en' }) => {
+export const getStaticProps = async ({ locale = "en" }) => {
   return {
     props: {
       ...(await serverSideTranslations(locale)),
@@ -26,10 +26,8 @@ const Custom404 = () => {
   return (
     <>
       <Head>
-        <title>{t('title')}
+        <title>{t("title")}</title>
         <link rel="preload" href="/custom404.svg" as="image" />
-        
-        </title>
       </Head>
       <div className="h-screen w-full bg-[#17002F] py-32 sm:p-32">
         <div className="relative mx-auto h-full w-[100%]">
@@ -45,7 +43,7 @@ const Custom404 = () => {
           onClick={handleClick}
           className="mx-auto flex h-[66px] w-[231px] items-center justify-center rounded-xl bg-[#73D677] text-2xl font-bold text-black shadow-[inset_0px_-9px_0px_rgba(0,0,0,0.18)] transition active:scale-95"
         >
-          {t('return')}
+          {t("return")}
         </button>
       </div>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,8 +32,8 @@ const Home: NextPageWithLayout = () => {
       <Head>
         <title>
           {`Star Wars - ${t("home")}`}
-          <link rel="preload" href="/hero.svg" as="image" />
         </title>
+          <link rel="preload" href="/hero.svg" as="image" />
       </Head>
       <div className="flex min-h-[calc(100vh-93px)] w-full flex-col items-center gap-10 bg-red-300 bg-gradient-to-b from-[#1F2A63] to-[#17002F] px-4 py-16 lg:flex-row lg:items-center lg:justify-center lg:gap-2 lg:py-0">
         <div className="w-full text-center text-white lg:w-[500px] lg:text-start">
